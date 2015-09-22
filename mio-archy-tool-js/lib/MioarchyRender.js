fad0a98a60a6748dd3e48b2669dfde24f1f69ef1
Victor Leipnik <vleipnik@miovision.com>
2015-09-22 13:16:55 -0400

    first refactor step

--- mio-archy-tool-js/lib/MioarchyRender.js
+++ mio-archy-tool-js/lib/MioarchyRender.js
@@ -1,92 +1,122 @@
 'use strict';
 
+function RenderingUtilities() {
+}
+
+// Generates a group of circle locations, with a minimum distance between the circles
+RenderingUtilities.calculateCircleLocations = function(numCircles, minDistanceBetweenCircles, circleDiameter, useMiddle) {
+    var dm = minDistanceBetweenCircles;
+    var rc = circleDiameter / 2;
+    var n = 0;
+    var Nc = numCircles;
+    var radiusIncrement = 1;
+    var Nc_current = 0;
+    var Nc_remaining = Nc;
+    var x = 0;
+    var y = 0;
+    var R = 0;
+    var alpha_min;
+    var alpha;
+    var minx = Number.MAX_VALUE;
+    var miny = Number.MAX_VALUE;
+    var maxx = Number.MIN_VALUE;
+    var maxy = Number.MIN_VALUE;
+    // will populate resulting object
+    var width = 0;
+    var height = 0;
+    var circleCenters;
+
+    // base cases
+    if (numCircles <= 0) {
+        width = 0;
+        height = 0;
+        circleCenters = [{}];
+    }
+    else if (numCircles == 1) {
+        width = circleDiameter;
+        height = circleDiameter;
+        circleCenters = [{x: 0, y: 0}];
+    } else {
+        circleCenters = [];
+
+        if (useMiddle) {
+            // first circle is in the middle
+            circleCenters[0] = {x: 0, y: 0};
+            Nc_remaining--;
+            n = 1;
+        }
+
+        while (Nc_remaining > 0) {
+            // calculate current radius for this iteration
+            R = radiusIncrement * (2 * rc + dm);
+
+            // how many circles do we need to fit on the outer edge?
+            alpha_min = 2 * Math.atan((rc + dm / 2) / R);
+
+            // use remaining number of circles to see if they would all fit
+            alpha = 2 * Math.PI / Nc_remaining;
+            if (alpha < alpha_min) {
+                // we have too many circles - so just use alpha min to determine the number of circles
+                Nc_current = 1 + Math.floor(2 * Math.PI / alpha_min);
+                alpha = 2 * Math.PI / Nc_current; // override alpha in case of error due to approximation used
+            } else {
+                Nc_current = Nc_remaining;
+            }
+
+            // now draw the # of circles, given alpha
+            for (var i = 0; i < Nc_current; i++) {
+                var angle = alpha * i;
+                var newX = x + R * Math.cos(angle);
+                var newY = y + R * Math.sin(angle);
+                var p = {x: newX, y: newY};
+                circleCenters[n++] = p;
+
+                // update boundaries, accounting for circle radius
+                maxx = Math.max(maxx, newX + rc);
+                maxy = Math.max(maxy, newY + rc);
+                minx = Math.min(minx, newX - rc);
+                miny = Math.min(miny, newY - rc);
+            }
+
+            // increase to next radius for next iteration
+            radiusIncrement++;
+            Nc_remaining -= Nc_current;
+        }
+        width = (R + rc) * 2;
+        height = width;
+    }
+    return {
+        width: width, height: height, circleCenters: circleCenters
+    };
+};
+
+// Moves a list of points by dx,dy
+RenderingUtilities.translatePoints = function(points, dx, dy) {
+    var pointsTranslated = [];
+    for (var p in points) {
+        pointsTranslated.push( { x: points[p].x + dx, y: points[p].y + dy } );
+    }
+    return pointsTranslated;
+};
+
+RenderingUtilities.calculateBoundingCircleDiameter = function(width, height) {
+    return Math.sqrt( width*width + height*height );
+};
+
 function RenderInfoCircles(numCircles, minDistanceBetweenCircles, circleDiameter, useMiddle)
 {
-    this.circleCenters = {};
-    this.calculateCircleLocations(numCircles, minDistanceBetweenCircles, circleDiameter, useMiddle);
+    var calculatedLocations = RenderingUtilities.calculateCircleLocations(numCircles, minDistanceBetweenCircles, circleDiameter, useMiddle);
+    this.width = calculatedLocations.width;
+    this.height = calculatedLocations.height;
+    this.circleCenters = calculatedLocations.circleCenters;
+
+    /*
+    // circle coordinates need to be offset to be translated to have 0,0 as the top-left
+    var dx = this.width / 2 + circleDiameter / 2;
+    var dy = this.height / 2 + circleDiameter / 2;
+    RenderingUtilities.translatePoints(this.circleCenters, dx, dy);*/
 }
-
-RenderInfoCircles.prototype = {
-    // Draws equally sized and spaced circles
-    calculateCircleLocations: function(numCircles, minDistanceBetweenCircles, circleDiameter, useMiddle) {
-        var dm = minDistanceBetweenCircles;
-        var rc = circleDiameter / 2;
-        var n = 0;
-        var Nc = numCircles;
-        var radiusIncrement = 1;
-        var Nc_current = 0;
-        var Nc_remaining = Nc;
-        var x = 0;
-        var y = 0;
-        var R = 0;
-        var alpha_min;
-        var alpha;
-        var minx = Number.MAX_VALUE;
-        var miny = Number.MAX_VALUE;
-        var maxx = Number.MIN_VALUE;
-        var maxy = Number.MIN_VALUE;
-
-        // base cases
-        if (numCircles <= 0) {
-            this.width = 0;
-            this.height = 0;
-            this.circleCenters = [ { } ];
-        }
-        else if (numCircles == 1) {
-            this.width = circleDiameter;
-            this.height = circleDiameter;
-            this.circleCenters = [ { x:0, y:0 } ];
-        } else {
-            this.circleCenters = [];
-
-            if (useMiddle) {
-                // first circle is in the middle
-                this.circleCenters[0] = { x:0, y:0 };
-                Nc_remaining--;
-                n = 1;
-            }
-
-            while (Nc_remaining > 0) {
-                // calculate current radius for this iteration
-                R = radiusIncrement * (2 * rc + dm);
-
-                // how many circles do we need to fit on the outer edge?
-                alpha_min = 2 * Math.atan((rc + dm / 2) / R);
-
-                // use remaining number of circles to see if they would all fit
-                alpha = 2 * Math.PI / Nc_remaining;
-                if (alpha < alpha_min) {
-                    // we have too many circles - so just use alpha min to determine the number of circles
-                    Nc_current = 1 + Math.floor( 2 * Math.PI / alpha_min );
-                    alpha = 2 * Math.PI / Nc_current; // override alpha in case of error due to approximation used
-                } else {
-                    Nc_current = Nc_remaining;
-                }
-
-                // now draw the # of circles, given alpha
-                for (var i = 0; i < Nc_current; i++) {
-                    var angle = alpha * i;
-                    var newX = x + R * Math.cos(angle);
-                    var newY = y + R * Math.sin(angle);
-                    var p = { x: newX, y: newY };
-                    this.circleCenters[n++] = p;
-
-                    // update boundaries, accounting for circle radius
-                    maxx = Math.max(maxx, newX + rc);
-                    maxy = Math.max(maxy, newY + rc);
-                    minx = Math.min(minx, newX - rc);
-                    miny = Math.min(miny, newY - rc);
-                }
-
-                // increase to next radius for next iteration
-                radiusIncrement++;
-                Nc_remaining -= Nc_current;
-            }
-            this.width = (R + rc) * 2;
-            this.height = this.width;
-        }
-    }
-}
+RenderInfoCircles.prototype = {};
 
 function RenderInfoOrganization(org, mioarchy)
 {
@@ -142,7 +172,7 @@ function processLeafRenderingInfo() {
         this.jobsAtThisLevel.length, this.MIN_DISTANCE_BETWEEN_CIRCLES, this.CIRCLE_DIAMETER, false );
 
     // circle
-    this.width = this.calculateBoundingCircleDiameter( this.circleForContributorsAtThisOrgLevel.width, this.circleForContributorsAtThisOrgLevel.height );
+    this.width = RenderingUtilities.calculateBoundingCircleDiameter( this.circleForContributorsAtThisOrgLevel.width, this.circleForContributorsAtThisOrgLevel.height );
     this.height = this.width;
 
     this.maxOrgCircleDiameter = this.width; // not really used, just being diligent
@@ -212,7 +242,7 @@ function processRectangularOrgRendering() {
 
 function renderJobsAtThisOrgLevel(x, y, dx, dy, graph) {
     // now move all the circle locations as needed
-    var circleLocations = this.translatePoints(this.circleForContributorsAtThisOrgLevel.circleCenters, dx, dy);
+    var circleLocations = RenderingUtilities.translatePoints(this.circleForContributorsAtThisOrgLevel.circleCenters, dx, dy);
 
     // render jobs (circles)
     for (var j in this.jobsAtThisLevel) {
@@ -383,7 +413,7 @@ RenderInfoOrganization.prototype =
              // if there are jobs, render them
              if (this.jobsAtThisLevel.length > 0) {
                  // now move all the circle locations as needed
-                 var circleLocations = this.translatePoints(this.circleForContributorsAtThisOrgLevel.circleCenters, dx, dy);
+                 var circleLocations = RenderingUtilities.translatePoints(this.circleForContributorsAtThisOrgLevel.circleCenters, dx, dy);
 
                  // render jobs (circles)
                  for (var j in this.jobsAtThisLevel) {
@@ -457,15 +487,5 @@ RenderInfoOrganization.prototype =
         }
         return colorString;
     },
-    // Moves a list of points by dx,dy
-    translatePoints: function(points, dx, dy) {
-        var pointsTranslated = [];
-        for (var p in points) {
-            pointsTranslated.push( { x: points[p].x + dx, y: points[p].y + dy } );
-        }
-        return pointsTranslated;
-    },
-    calculateBoundingCircleDiameter: function(width, height) {
-        return Math.sqrt( width*width + height*height );
-    },
+
 }
