06a1e0cee32f61ff654d3352a2867655a5e0c96c
Victor Leipnik <vleipnik@miovision.com>
2015-12-17 22:56:54 -0500

    tweaked footer and feedback

--- od-tool/App.jsx
+++ od-tool/App.jsx
@@ -22,11 +22,27 @@ App = React.createClass({
                 <br/>
 
                 <footer className="page-footer teal">
+					<div className="container">
+						<div className="row">
+							<div className="col s9">
+								<h5 className="white-text">Help us develop this software</h5>
+								<p className="grey-text text-lighten-4">"Your feedback can help us build this software
+									to serve our organization as we scale. Please assist us by reporting bugs,
+									suggesting features, or just letting us know how we're doing" - Vic</p>
+
+							</div>
+							<div className="col s3">
+								<br />
+								<button className="btn waves-effect waves-light teal lighten-3 center"
+										onClick={this.handleFeedbackClick}>Provide Feedback
+								</button>
+							</div>
+						</div>
+					</div>
+
                     <div className="footer-copyright">
                         <div className="container">
                             © 2016 Miovision OD Tool
-                            <a className="grey-text text-lighten-4 right modal-trigger"
-							   onClick={this.handleFeedbackClick}>Provide Feedback</a>
                         </div>
                     </div>
                 </footer>
