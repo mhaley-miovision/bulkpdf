a4a4026c32e28c9daa3e0cfea5718fdbcc673096
Victor Leipnik <vleipnik@miovision.com>
2016-02-15 18:15:37 -0500

    goals now show as awesome donut chart... ohh yeaa

--- od-tool/client/Components/SummaryCards/GoalsSummary.jsx
+++ od-tool/client/Components/SummaryCards/GoalsSummary.jsx
@@ -27,8 +27,9 @@ GoalsSummary = React.createClass({
 			});
 
 			let summaryGoal = { name: "Goal completion", stats: { notStarted:notStarted, inProgress:inProgress, completed:completed }  };
-
-			return { doneLoading: true, goals: goals, summaryGoal: summaryGoal, lateGoals: lateGoals }
+			this.data = { doneLoading: true, goals: goals, summaryGoal: summaryGoal, lateGoals: lateGoals };
+			this.updateChart();
+			return this.data;
 		} else {
 			return { doneLoading: false };
 		}
@@ -98,6 +99,96 @@ GoalsSummary = React.createClass({
 		}
 	},
 
+	updateChart() {
+		let _this = this;
+		setTimeout(function () {
+			// Get context with jQuery - using jQuery's .get() method.
+			var ctx = $("#goalDonutChart").get(0).getContext("2d");
+
+			// This will get the first returned node in the jQuery collection.
+			var c = new Chart(ctx);
+
+			/*
+			Chart.types.Doughnut.extend({
+				name: "DoughnutAlt",
+				draw: function() {
+					Chart.types.Doughnut.prototype.draw.apply(this, arguments);
+					this.chart.ctx.fillStyle = 'black';
+					this.chart.ctx.font = "16pt Arial";
+					this.chart.ctx.textBaseline = 'middle';
+
+					//TODO: hack because I'm tired and impatient with this fidgetting at this point
+					var xOffset = 30;
+					if (this.segments[0].value >= 10) {
+						xOffset = 21;
+					} else if (this.segments[0].value === 100) {
+						xOffset = 17;
+					}
+
+					this.chart.ctx.fillText(this.segments[0].value + "%", 30, 40, 140);
+				}
+			});
+			*/
+
+			var g = _this.data.summaryGoal;
+			var data = [
+				{
+					value: g.stats.completed,
+					color:"#46BFBD",
+					highlight: "#FF5A5E",
+					label: "Completed"
+				},
+				{
+					value: g.stats.inProgress,
+					color:"#FDB45C",
+					highlight: "#FFC870",
+					label: "In Progress"
+				},
+				{
+					value: g.stats.notStarted,
+					color: "#ff6666",
+					highlight: "#FF5A5E",
+					label: "Not Started",
+				},
+			];
+
+			var ctx = $('#goalDonutChart').get(0).getContext("2d");
+
+			var myDoughnut = new Chart(ctx).Doughnut(data,{
+				animation:true,
+				animationSteps: 50,
+				responsive: true,
+				showTooltips: false,
+				percentageInnerCutout : 80,
+				segmentShowStroke : false,
+				onAnimationComplete: function() {
+					var canvasWidthvar = $('#goalDonutChart').width();
+					var canvasHeight = $('#goalDonutChart').height();
+					//this constant base on canvasHeight / 2.8em
+					var constant = 104;
+					var fontsize = (canvasHeight/constant).toFixed(2);
+					ctx.font=fontsize +"em Verdana";
+					ctx.textBaseline="middle";
+					ctx.fillStyle = "#999999";
+					var total = 0;
+					$.each(data,function() {
+						total += parseInt(this.value,10);
+					});
+					var tpercentage = ((data[0].value/total)*100).toFixed(2)+"%";
+					var textWidth = ctx.measureText(tpercentage).width;
+
+					var txtPosx = Math.round((canvasWidthvar - textWidth)/2);
+					ctx.fillText(tpercentage, txtPosx, canvasHeight/2);
+				}
+			});
+		}, 50);
+	},
+
+	getCompleteAsPercentString() {
+		var total = this.data.summaryGoal.stats.completed + this.data.summaryGoal.stats.inProgress + this.data.summaryGoal.stats.notStarted;
+		return Math.ceil(this.data.summaryGoal.stats.completed / total * 100) + "%";
+	},
+
 	render() {
 		if (this.data.doneLoading) {
 			return (
@@ -106,9 +197,9 @@ GoalsSummary = React.createClass({
 						<li className="collection-header summaryCardHeader">Goals</li>
 
 						<li className="collection-item">
-							<div className="collection-item-text">Completion progress ({this.data.goals.length} goals): </div>
-							<div className="right">
-								<SimpleGoalProgressBar margin="6px 0 0 0" goal={this.data.summaryGoal}/>
+							<div style={{width:"120px", height: "120px"}} className="goalDonutChart">
+								<canvas id="goalDonutChart" width="90" height="90">
+								</canvas>
 							</div>
 						</li>
 						<li className="collection-item">
