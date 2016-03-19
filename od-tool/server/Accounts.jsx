ac790a65244b0b7a2a1f0dfcce69d1e91ed91175
Victor Leipnik <vleipnik@miovision.com>
2016-03-19 14:17:21 -0400

    fixed security gap

--- od-tool/server/Accounts.jsx
+++ od-tool/server/Accounts.jsx
@@ -12,22 +12,25 @@ Accounts.validateNewUser(function (user) {
 			// if there is a contributor associated, allow it
 			let c = ContributorsCollection.findOne({email:email});
 			if (c) {
+				console.log(`Accounts.validateNewUser('${u._id}') - found contributor - '${c.name}'`);
 				return true;
 			}
 
 			// custom enabled users
 			var enabledUsers = ['leipnik@gmail.com'];
 			if (enabledUsers.indexOf(email) >= 0) {
+				console.log(`Accounts.validateNewUser('${u._id}') - used part of custom enabled users - '${email}'`);
 				return true;
 			}
 
 			// enable all miovision users by default
-			if (email.indexOf("@miovision.com")) {
+			if (email.indexOf("@miovision.com") >= 0) {
+				console.log(`Accounts.validateNewUser('${u._id}') - user is Miovision employee - '${email}'`);
 				return true;
 			}
 		}
 	}
-	console.error("Accounts.validateNewUser FAILED -- ");
-	console.log(user);
+	console.error("Accounts.validateNewUser FAILED");
+	console.error(user);
 	throw new Meteor.Error(403, "This user is not enabled.");
 });
